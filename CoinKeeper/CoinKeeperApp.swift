26fcc5a012b5fe65667e5293229a5a7baaf523c8
Vlad <vladislav.bok@yandex.ru>
2022-06-10 17:19:31 +0300

    Initial Commit

---
+++ CoinKeeper/CoinKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinKeeperApp.swift
+//  CoinKeeper
+//
+//  Created by Влад Бокин on 10.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoinKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
